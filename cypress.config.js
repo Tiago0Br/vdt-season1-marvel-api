61b60b337179d0c95597bb1ee8f8bd1fae7927d7
Tiago Tavares Lopes <tiagoltavares2002@gmail.com>
2022-06-11 21:39:38 -0300

    Baixando e configurando o Cypress

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://marvel-qa-cademy.herokuapp.com',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
+  }
+})
